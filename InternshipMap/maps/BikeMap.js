62e7b299d24357d0e5270299a892dd3dce5edddf
petermanos <72958049+petermanos@users.noreply.github.com>
2022-12-12 23:20:47 -0500

    Add files via upload

---
+++ InternshipMap/maps/BikeMap.js
@@ -0,0 +1,28 @@
+var map = L.map('map', {
+  center: [40.7128, -74.0060],
+  minZoom: 2,
+  zoom: 10,
+})
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  subdomains: ['a', 'b', 'c'],
+}).addTo(map)
+
+var myURL = jQuery('script[src$="leaf-demo.js"]')
+  .attr('src')
+  .replace('leaf-demo.js', '')
+
+var myIcon = L.icon({
+  iconUrl: myURL + 'images/pin24.png',
+  iconRetinaUrl: myURL + 'images/pin48.png',
+  iconSize: [29, 24],
+  iconAnchor: [9, 21],
+  popupAnchor: [0, -14],
+})
+
+for (var i = 0; i < markers.length; ++i) {
+  L.markers(markers.the_geom.coordinates(), { icon: myIcon })
+    .addTo(map)
+}
